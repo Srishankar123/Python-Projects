d742f922eb4d0f8cc9da53822f4f1cd70bbfd760
Srishankar123 <76104274+Srishankar123@users.noreply.github.com>
2025-05-12 15:37:27 +0530

    Update guessing the number.py

--- guessing the number.py
+++ guessing the number.py
@@ -1,28 +1,52 @@
 import random
 
-min=int(input("Enter the min number for guessing game: "))
-max=int(input("Enter the max number for guessing game: "))
-rand1=random.randint(min,max)
-print(rand1)
-count=0
-
-
+flag= None
+Min = int(input("Enter the min number for guessing game: "))
+Max = int(input("Enter the max number for guessing game: "))
+rand1 = random.randint(Min, Max)
+count = 0
+best = None
 while True:
-    num=int(input(f"Guess the number (between {min} and {max}): "))
+    if flag == True:
+        Min = int(input("Enter the min number for guessing game: "))
+        Max = int(input("Enter the max number for guessing game: "))
+        rand1 = random.randint(Min, Max)
+        print(rand1)
+        flag = None
+        count=0
 
-    if num<rand1:
-        print("Too low! Try again.")
-        count+=1
-
-    elif num>rand1:
-        print("Too high! Try again.")
-        count+=1
-
-    elif num==rand1:
-        print(f"Congratulations! You guessed the number in {count} attempts.")
+    if flag == False:
+        print("Good Bye!!!")
         break
 
-    if count > 10:
-        print("Exceeded Maximum Guesses")
-        break
+    while True:
+        num=int(input(f"Guess the number (between {Min} and {Max}): "))
 
+        if num<rand1:
+            print("Too low! Try again.")
+            count+=1
+
+        elif num>rand1:
+            print("Too high! Try again.")
+            count+=1
+
+        elif num==rand1:
+            if best is None or count < best:
+                best = count
+                print("if works")
+            print(f"Congratulations! You guessed the number in {count} attempts.\n Your Lowest Guess record is : {best}")
+            if input("Whether you want to play the game? (y/n): ") == "y":
+                flag = True
+                break
+            else:
+                flag = False
+                break
+
+        if count > 10:
+            print("Exceeded Maximum Guesses")
+            if input("Whether you want to play the game? (y/n): ") == "y":
+                flag = True
+                break
+            else:
+                flag = False
+                break
